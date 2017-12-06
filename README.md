b13154fbd8e647e469e1f6b9790e5f5c7ac0528a
ae6623 <394263788@qq.com>
2017-12-06 22:26:36 +0800

    add add react-comment-header

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # ReactL
-ReactL-router-redux
 
-# npm i
-# npm run hot
+This repo is the study for ReactL-router-redux, you can use react from 0 to 1 with my repo.
 
+## version
+---- 
+Nodejs v8.6
+Npm v5.3.0
+Reactjs 15.x -> 16.x later
+Webpack 1.x -> 3.x later
+
+```
+cd ReactL
+npm i
+npm run hot
+
+```
+
+## tag
 ----
-2017-12-4 23:40:06 add webpack hot
+tag v1.0 2017-12-4 23:40:06 add webpack hot
+tag v1.1 2017-12-6 22:12:41 add react-comment-header
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,16 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ComponentHeader from './commonts/head'
 
-ReactDOM.render(
-  <h1>Hello hot code</h1>,
-  document.getElementById('example')
-);
+class Index extends React.Component{
+  render(){
+    return (
+      <div> 
+        <ComponentHeader/>
+        <h2>v1.1 component-header</h2>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Index/>,document.getElementById('example'));
